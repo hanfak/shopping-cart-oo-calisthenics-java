781438f92696127a2163a4e647d6d6e4d0a2c078
Hanif Fakira <fakira.work@gmail.com>
2017-03-12 21:34:14 +0000

    Tidiy test

--- src/test/java/shoppingcart/CheckoutTests.java
+++ src/test/java/shoppingcart/CheckoutTests.java
@@ -14,6 +14,24 @@ public class CheckoutTests {
         thenTotalWillBe0();
     }
 
+    @Test
+    public void totalIs50WhenNoItemAIsScanned() {
+        givenACheckoutSystemIsActive();
+
+        whenItemAisScanned();
+
+        thenTotalIs50();
+    }
+
+    @Test
+    public void totalIs30WhenNoItemBIsScanned() {
+        givenACheckoutSystemIsActive();
+
+        whenItemBIsScanned();
+
+        thenTotalIs30();
+    }
+    
     private void givenACheckoutSystemIsActive() {
         checkout = new Checkout();
     }
@@ -22,19 +40,25 @@ public class CheckoutTests {
         assertEquals(new Money(0), checkout.total());
     }
 
-    @Test
-    public void totalIs50WhenNoItemAIsScanned() {
-        Checkout checkout = new Checkout();
-        checkout.scan(new Item("A"));
-
+    private void thenTotalIs50() {
         assertEquals(new Money(50), checkout.total());
     }
 
-    @Test
-    public void totalIs30WhenNoItemBIsScanned() {
-        Checkout checkout = new Checkout();
-        checkout.scan(new Item("B"));
+    private void whenItemAisScanned() {
+        checkout.scan(new Item("A"));
+    }
 
+    private void thenTotalIs30() {
         assertEquals(new Money(30), checkout.total());
     }
+
+    private void whenItemBIsScanned() {
+        checkout.scan(new Item("B"));
+    }
+
+
+    //test multiple different no discount
+    //test multiple same item with discount
+    //test multiple other item different discount
+    //test mulitple items with and without discounts
 }
