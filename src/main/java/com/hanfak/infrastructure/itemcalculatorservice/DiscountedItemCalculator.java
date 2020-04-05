b211928c4541b008cc66e549dae0d9482be92e73
Hanif Fakira <fakira.work@gmail.com>
2020-04-05 11:22:48 +0100

    refactoring - use internal map for discounts, to allow for multiple extensions for more items

--- src/main/java/com/hanfak/infrastructure/itemcalculatorservice/DiscountedItemCalculator.java
+++ src/main/java/com/hanfak/infrastructure/itemcalculatorservice/DiscountedItemCalculator.java
@@ -4,24 +4,38 @@ import com.hanfak.domain.Item;
 import com.hanfak.repository.ScannedItemsRepository;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DiscountedItemCalculator {
   // TODO inject DiscountedItemRepoistory (interface)
   // TODO inject rules engined, which injects DiscountedItemRepoistory
+  private static final Map<Item, DiscountRule> discounts = new HashMap<>();
 
-  public BigDecimal calculate(ScannedItemsRepository scannedItems) {
-    BigDecimal itemADiscountedTotal = calulateTotalOfDiscountedItems(scannedItems, "A", 3, 20L);
-    BigDecimal itemBDiscountedTotal = calulateTotalOfDiscountedItems(scannedItems, "B", 2, 15L);
-    return itemADiscountedTotal.add(itemBDiscountedTotal);
+  static {
+    // TODO calculate discount amount from item price???
+    discounts.put(new Item("A", BigDecimal.valueOf(50L)), new DiscountRule(3, 20L));
+    discounts.put(new Item("B", BigDecimal.valueOf(30L)), new DiscountRule(2, 15L));
   }
 
-  private BigDecimal calulateTotalOfDiscountedItems(ScannedItemsRepository scannedItems, String a, int i, long l) {
-    int totalOfItemA = scannedItems.numberOfScannedItem(a);
-    int numberOfGroups = totalOfItemA / i; // extract to ScannedItemsRepository
-    int numberOfLeftOver = totalOfItemA % i; // extract to ScannedItemsRepository
-    BigDecimal totalOfItemsToBeDiscounted = calculateGroupTotalOfItems(scannedItems, a, totalOfItemA - numberOfLeftOver);
-    BigDecimal totalIncludingDiscount = totalOfItemsToBeDiscounted.subtract(BigDecimal.valueOf(l).multiply(BigDecimal.valueOf(numberOfGroups)));
-    BigDecimal totalOfItemsToBeNotDiscounted = calculateGroupTotalOfItems(scannedItems, a, numberOfLeftOver);
+  public BigDecimal calculate(ScannedItemsRepository scannedItems) {
+    return discounts.entrySet().stream()
+            .map(discount -> calulateTotalOfDiscountedItems(scannedItems,
+                    discount.getKey().getName(),
+                    discount.getValue().amountOfItemsToApplyDiscount,
+                    discount.getValue().discountAmount))
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
+
+  private BigDecimal calulateTotalOfDiscountedItems(ScannedItemsRepository scannedItems, String itemName, int itemAmountToApplyDiscountTo, long discountAmount) {
+    int totalOfItemA = scannedItems.numberOfScannedItem(itemName);
+    int numberOfGroups = totalOfItemA / itemAmountToApplyDiscountTo; // extract to ScannedItemsRepository
+    int numberOfLeftOver = totalOfItemA % itemAmountToApplyDiscountTo; // extract to ScannedItemsRepository
+    BigDecimal totalOfItemsToBeDiscounted = calculateGroupTotalOfItems(scannedItems, itemName, totalOfItemA - numberOfLeftOver);
+    BigDecimal totalIncludingDiscount = totalOfItemsToBeDiscounted.subtract(BigDecimal.valueOf(discountAmount).multiply(BigDecimal.valueOf(numberOfGroups)));
+    BigDecimal totalOfItemsToBeNotDiscounted = calculateGroupTotalOfItems(scannedItems, itemName, numberOfLeftOver);
     return totalIncludingDiscount.add(totalOfItemsToBeNotDiscounted);
   }
 
@@ -29,5 +43,15 @@ public class DiscountedItemCalculator {
     return scannedItems.itemsOfLength(limit, itemName).stream()
             .map(Item::getPrice)
             .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
+
+  private static class DiscountRule {
+    private final int amountOfItemsToApplyDiscount;
+    private final long discountAmount;
+
+    public DiscountRule(int amountOfItemsToApplyDiscount, long discountAmount) {
+      this.amountOfItemsToApplyDiscount = amountOfItemsToApplyDiscount;
+      this.discountAmount = discountAmount;
+    }
   }
 }
